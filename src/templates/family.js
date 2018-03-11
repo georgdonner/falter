0112ae8c41aab6a95d5e6695492beb866367abc7
Georg Donner <georgdonner98@gmail.com>
2018-03-11 23:59:03 +0100

    Add styles for falter family overview.

--- src/templates/family.js
+++ src/templates/family.js
@@ -2,26 +2,33 @@ import React from 'react';
 import graphql from 'graphql';
 import Helmet from 'react-helmet';
 import Link from 'gatsby-link';
+import Img from 'gatsby-image';
+import './family.scss';
 
 const Family = ({ data }) => {
-  const { edges, totalCount } = data.allMarkdownRemark;
+  const { edges } = data.allMarkdownRemark;
   const { familyName } = edges[0].node.frontmatter;
-  const familyHeader = `${totalCount} Falter der Familie "${familyName}"`;
 
   return (
     <div>
       <Helmet title={`Falter - ${familyName}`} />
-      <h1>{familyHeader}</h1>
-      <ul>
+      <h1 id="family-title">{familyName}</h1>
+      <div id="family-container">
         {edges.map(({ node }) => {
-          const { path, name } = node.frontmatter;
+          const {
+            path, name, nameLatin, images,
+          } = node.frontmatter;
           return (
-            <li key={path}>
-              <Link to={path}>{name}</Link>
-            </li>
+            <div key={path} className="falter-card">
+              <Link to={path}>
+                <Img sizes={images[0].src.childImageSharp.sizes} imgStyle={{ borderRadius: '5px 5px 0 0' }} />
+                <h2>{name}</h2>
+                <h3><i>{nameLatin}</i></h3>
+              </Link>
+            </div>
           );
         })}
-      </ul>
+      </div>
     </div>
   );
 };
@@ -32,16 +39,25 @@ export const pageQuery = graphql`
   query FamilyPage($family: String!) {
     allMarkdownRemark(
       limit: 2000
-      sort: { fields: [frontmatter___name], order: DESC }
+      sort: { fields: [frontmatter___name], order: ASC }
       filter: { frontmatter: { family: { eq: $family } } }
     ) {
-      totalCount
       edges {
         node {
           frontmatter {
             path
             name
+            nameLatin
             familyName
+            images {
+              src {
+                childImageSharp {
+                  sizes(maxWidth: 600) {
+                    ...GatsbyImageSharpSizes
+                  }
+                }
+              }
+            }
           }
         }
       }
