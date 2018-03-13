4d04c4c138ca6ca4e27b28fca0c03071e4f5ad5a
Georg Donner <georgdonner98@gmail.com>
2018-03-13 22:23:57 +0100

    Add favicons. Add manifest. Add offline service workers.

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,47 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: './src/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Falter',
+        short_name: 'Falter',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#e84b4b',
+        display: 'minimal-ui',
+        icons: [
+          {
+            src: '/favicons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/favicons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-catch-links',
     {
@@ -33,5 +74,6 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-offline',
   ],
 };
