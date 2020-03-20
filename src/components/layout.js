6116db305fb9034bff5da73394b18dbbc5711c12
Georg Donner <georgdonner@posteo.de>
2020-03-20 13:51:02 +0100

    Convert layout to functional component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import Helmet from 'react-helmet';
 import { Swipeable } from 'react-swipeable';
@@ -32,80 +32,81 @@ export const LayoutContext = React.createContext({
   setSidebar: () => {},
 });
 
-export default class TemplateWrapper extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { sidebar: false };
-    this.setSidebar = (sidebar = true) => this.setState({ sidebar });
-  }
+const usePrevious = (value) => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
 
-  componentWillReceiveProps(nextProps) {
-    const { location } = this.props;
-    if (location.pathname !== nextProps.location.pathname) {
-      this.setState({ sidebar: false });
+export default ({ location, children }) => {
+  const [sidebar, setSidebar] = useState(false);
+
+  const prevLocation = usePrevious(location);
+  useEffect(() => {
+    if (prevLocation && location.pathname !== prevLocation.pathname) {
+      setSidebar(false);
     }
-  }
+  }, [location]);
 
-  render() {
-    const { children, location } = this.props;
-    const { sidebar } = this.state;
-    const pageContent = (data) => {
-      const { edges } = data.allMarkdownRemark;
-      return (
-        <Fragment>
-          <Helmet
-            title="Falter"
-            meta={[
-              { name: 'description', content: 'Tagfalter Deutschlands' },
-              { name: 'keywords', content: 'falter, schmetterlinge, tagfalter' },
-            ]}
-          />
-          <div id="root">
-            <div id="sidebar-wrapper-outer" className={sidebar ? 'active' : ''}>
-              <Swipeable
-                onSwipedLeft={() => {
-                  if (sidebar) this.setSidebar(false);
-                }}
-              >
-                <Sidebar
-                  falters={edges.map(edge => edge.node.frontmatter)}
-                  path={location.pathname}
-                />
-              </Swipeable>
-            </div>
-            {/* eslint-disable-next-line */}
-              <div id="main-wrapper"
-                onClick={() => {
-                  if (sidebar) this.setSidebar(false);
-                }}
-              >
-                <div
-                  role="menu"
-                  tabIndex="0"
-                  id="sidebar-toggle"
-                  onClick={() => this.setSidebar(!sidebar)}
-                  onKeyPress={(event) => {
-                    if (event.key.toLowerCase() === 'enter') {
-                      this.setSidebar(!sidebar);
-                    }
-                  }}
-                  onMouseDown={(e) => { e.preventDefault(); }}
-                >
-                  <FontAwesomeIcon icon={faBars} />
-                </div>
-                <LayoutContext.Provider value={{ sidebar, setSidebar: this.setSidebar }}>
-                  {children}
-                </LayoutContext.Provider>
-              </div>
-          </div>
-        </Fragment>
-      );
-    };
+  const pageContent = (data) => {
+    const { edges } = data.allMarkdownRemark;
     return (
-      <StaticQuery
-        query={pageQuery}
-        render={pageContent}
-      />
+      <>
+        <Helmet
+          title="Falter"
+          meta={[
+            { name: 'description', content: 'Tagfalter Deutschlands' },
+            { name: 'keywords', content: 'falter, schmetterlinge, tagfalter' },
+          ]}
+        />
+        <div id="root">
+          <div id="sidebar-wrapper-outer" className={sidebar ? 'active' : ''}>
+            <Swipeable
+              onSwipedLeft={() => {
+                if (sidebar) setSidebar(false);
+              }}
+            >
+              <Sidebar
+                falters={edges.map(edge => edge.node.frontmatter)}
+                path={location.pathname}
+              />
+            </Swipeable>
+          </div>
+          {/* eslint-disable-next-line */}
+            <div id="main-wrapper"
+              onClick={() => {
+                if (sidebar) setSidebar(false);
+              }}
+            >
+              <div
+                role="menu"
+                tabIndex="0"
+                id="sidebar-toggle"
+                onClick={() => setSidebar(!sidebar)}
+                onKeyPress={(event) => {
+                  if (event.key.toLowerCase() === 'enter') {
+                    setSidebar(!sidebar);
+                  }
+                }}
+                onMouseDown={(e) => { e.preventDefault(); }}
+              >
+                <FontAwesomeIcon icon={faBars} />
+              </div>
+              <LayoutContext.Provider value={{ sidebar, setSidebar }}>
+                {children}
+              </LayoutContext.Provider>
+            </div>
+        </div>
+      </>
     );
-  }
-}
+  };
+
+  return (
+    <StaticQuery
+      query={pageQuery}
+      render={pageContent}
+    />
+  );
+};
